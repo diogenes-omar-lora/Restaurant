6736a3df0803f98a25a17cecb89eea195448b7d2
diogenes-omar-lora <omarlora08@hotmail.com>
2023-03-13 00:37:35 -0400

    imagenes

--- script/script.js
+++ script/script.js
@@ -1,6 +1,17 @@
+//title
+let docTitle = document.title;
+window.addEventListener( "blur",()=>{
+    document.title = "Come Back ;(";
+});
+
+window.addEventListener("focus", ()=>{
+    document.title = docTitle;
+});
+//menubottom
 let btn = document.querySelector("#checkbox");
 let nav = document.querySelector(".nav-menu");
 
 btn.onclick = function(){
     nav.classList.toggle("active");
 }
+//carrusel-sidebar
