9fe48cab43d78359e17026747a2e24e916d351de
diogenes-omar-lora <omarlora08@hotmail.com>
2023-04-04 20:52:49 -0400

    mas o menos bien

--- script/EnvioDatos.js
+++ script/EnvioDatos.js
@@ -1,6 +1,56 @@
 let Enviar = document.getElementById('boton');
 
-Enviar.addEventListener("click", function(){
+// Función para crear la tabla con los datos de las reservaciones
+function cargarTabla() {
+    let Reservaciones;
+
+    if (localStorage.getItem("reserva")) {
+      Reservaciones = JSON.parse(localStorage.getItem("reserva"));
+    } else {
+      Reservaciones = [];
+    }
+
+    let tbody = document.getElementById('tbody');
+    tbody.innerHTML = "";
+    Reservaciones.forEach(reserva => {
+      let tr = document.createElement('tr');
+
+      let td = document.createElement('td');
+      td.innerText = reserva.name;
+      td.setAttribute('data-label', 'First Name');
+      tr.appendChild(td);
+
+      const phone = document.createElement('td');
+      phone.innerText = reserva.phone;
+      phone.setAttribute('data-label', 'Phone');
+      tr.appendChild(phone);
+
+      const people = document.createElement('td');
+      people.innerText = reserva.people;
+      people.setAttribute('data-label', 'Peoples');
+      tr.appendChild(people);
+
+      const date = document.createElement('td');
+      date.innerText = reserva.date;
+      date.setAttribute('data-label', 'Date');
+      tr.appendChild(date);
+
+      const time = document.createElement('td');
+      time.innerHTML = reserva.time;
+      time.setAttribute('data-label', 'Time');
+      tr.appendChild(time);
+
+      tbody.appendChild(tr);
+    });
+  }
+
+  // Llamada a la función para crear la tabla al cargar la página
+  window.onload = function() {
+    cargarTabla();
+  }
+
+  // Evento click del botón Enviar
+  Enviar.addEventListener("click", function(){
     let name = document.getElementById('name').value;
     let phone = document.getElementById('phone').value;
     let people = document.getElementById('people').value;
@@ -9,37 +59,40 @@ Enviar.addEventListener("click", function(){
 
     if(name !='' && phone !='' && people !='' && date !='' && time !=''){
 
-        let Reservaciones;
+      let Reservaciones;
 
-        if (localStorage.getItem("reserva")) {
-            Reservaciones = JSON.parse(localStorage.getItem("reserva"));
-        } else {
-            Reservaciones = [];
-        }
+      if (localStorage.getItem("reserva")) {
+        Reservaciones = JSON.parse(localStorage.getItem("reserva"));
+      } else {
+        Reservaciones = [];
+      }
 
-        // Agregar un nuevo objeto a la lista de reservaciones
-        Reservaciones.push({
-            name: name,
-            phone: phone,
-            people: people,
-            date: date,
-            time: time,
-        });
+      // Agregar un nuevo objeto a la lista de reservaciones
+      Reservaciones.push({
+        name: name,
+        phone: phone,
+        people: people,
+        date: date,
+        time: time,
+      });
 
-        // Guardar la lista de reservaciones en el LocalStorage
-        localStorage.setItem("reserva", JSON.stringify(Reservaciones));
+      // Guardar la lista de reservaciones en el LocalStorage
+      localStorage.setItem("reserva", JSON.stringify(Reservaciones));
 
-        document.getElementById('name').value='';
-        document.getElementById('phone').value='';
-        document.getElementById('people').value='';
-        document.getElementById('date').value='';
-        document.getElementById('time').value='';
+      // Recargar la tabla con los nuevos datos
+      cargarTabla();
 
-    }else{
-        alert('No puede dejar los campos vacios...');
+      document.getElementById('name').value='';
+      document.getElementById('phone').value='';
+      document.getElementById('people').value='';
+      document.getElementById('date').value='';
+      document.getElementById('time').value='';
+
+    } else {
+      alert('No puede dejar los campos vacios...');
     }
-})
-
+  })
 document.getElementById("miFormulario").addEventListener("submit", function(event) {
     event.preventDefault();
   });
+
